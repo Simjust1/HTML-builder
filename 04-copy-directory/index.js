148fe8dc43d6f6d0353085c6207a0ed5c75d4eaa
Simjust1 <simjust@mail.ru>
2023-05-08 20:34:36 +0500

    feat: add code for the task 04 copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs/promises');
+const path = require('path');
+const pathForDir = path.join(__dirname, 'files-copy');
+const pathFromDir = path.join(__dirname, 'files');
+
+  fs.mkdir(pathForDir, { recursive: true }, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+  })
+
+  const ss = fs.readdir(pathFromDir, {withFileTypes: true}).then (data => {
+    for (let key of data) {
+      fs.copyFile(pathFromDir + '/' + key.name, pathForDir + '/' + key.name);
+    }
+  });
+
+
 
