3b0837118ace40424fccad5eaf0fa796f9f808cb
Simjust1 <simjust@mail.ru>
2023-05-09 17:54:15 +0500

    feat: add code when you delete a file inside the source directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,48 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 const pathForDir = path.join(__dirname, 'files-copy');
 const pathFromDir = path.join(__dirname, 'files');
 
-  fs.mkdir(pathForDir, { recursive: true }, (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-  })
+  fs.rmdir(pathForDir, { recursive: true, force: true }, (err) => {
+      fs.mkdir(pathForDir, { recursive: true }, (err) => {
+        if (err) {
+          console.error(err);
+        }
+        else {
+          createFiles();
+        }
+      })
+    })
 
-  const ss = fs.readdir(pathFromDir, {withFileTypes: true}).then (data => {
+  const createFiles = () => {
+    fs.readdir(pathFromDir, (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        console.log("\nCurrent directory filenames:");
+        files.forEach(file => {
+          console.log(file);
+          fs.copyFile(pathFromDir + '/' + file, pathForDir + '/' + file, (err) => {
+            if (err) {
+              console.log('error');
+            }
+            else {
+              console.log(file);
+            }
+          });
+        })
+      }
+    })
+  }
+
+
+
+
+/*   const ss = fs.readdir(pathFromDir, {withFileTypes: true} ).then (data => {
     for (let key of data) {
       fs.copyFile(pathFromDir + '/' + key.name, pathForDir + '/' + key.name);
     }
-  });
+  }); */
 
 
 
